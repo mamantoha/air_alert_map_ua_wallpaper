167fb79138b8af58e8cbaf50cc8c7b5ccf1972db
Anton Maminov <anton.linux@gmail.com>
2023-02-26 14:22:00 +0200

    extract code of browser functionality

--- src/air_alert_map_ua_wallpaper.cr
+++ src/air_alert_map_ua_wallpaper.cr
@@ -1,67 +1,60 @@
 require "selenium"
 
-# module AirAlertMapUaWallpaper
-#   VERSION = "0.1.0"
-# end
+module AirAlertMapUaWallpaper
+  class Browser
+    @session : Selenium::Session
 
-# This scripts takes a screenshot of https://alerts.in.ua and set it as a desktop backgroud.
-# Currently support only KDE Plasma.
-# Requires installed chromedriver, qdbus.
+    def initialize(driver_path = "/usr/bin/chromedriver", width = 2560, height = 1440)
+      service = Selenium::Service.chrome(driver_path: driver_path)
+      driver = Selenium::Driver.for(:chrome, service: service)
+      capabilities = Selenium::Chrome::Capabilities.new
+      capabilities.chrome_options.args = ["no-sandbox", "headless", "disable-gpu"]
 
-# Desktop resolution
-width = 2560
-height = 1440
+      @session = driver.create_session(capabilities)
+      @session.window_manager.resize_window(width, height)
+    end
 
-driver_path = "/usr/bin/chromedriver"
+    def take_screenshot : File
+      # map_url = "https://alerts.in.ua/en"
+      map_url = "https://alerts.in.ua"
 
-service = Selenium::Service.chrome(driver_path: driver_path)
-driver = Selenium::Driver.for(:chrome, service: service)
-capabilities = Selenium::Chrome::Capabilities.new
-capabilities.chrome_options.args = ["no-sandbox", "headless", "disable-gpu"]
-session = driver.create_session(capabilities)
+      @session.navigate_to(map_url)
 
-session.window_manager.resize_window(width, height)
+      # TODO check if site is loaded
+      #
+      # https://github.com/matthewmcgarvey/selenium.cr/pull/25
+      #
+      # wait = Selenium::Helpers::Wait.new(timeout: 5.seconds, interval: 1.second)
+      # wait.until { @session.find_element(:css, "#map svg") }
+      sleep 5.seconds
 
-# https://alerts.in.ua/?minimal&disableInteractiveMap
-# map_url = "https://alerts.in.ua/en"
-map_url = "https://alerts.in.ua"
+      element = @session.find_element(:css, "#map")
 
-session.navigate_to(map_url)
+      document_manager = @session.document_manager
 
-# TODO check if site is loaded
-#
-# https://github.com/matthewmcgarvey/selenium.cr/pull/25
-#
-# wait = Selenium::Helpers::Wait.new(timeout: 5.seconds, interval: 1.second)
-# wait.until { session.find_element(:css, "#map svg") }
-sleep 5.seconds
+      # Switch to dark theme
+      document_manager.execute_script("document.getElementsByTagName('html')[0].classList.toggle('light')")
 
-element : Selenium::Element = begin
-  session.find_element(:css, "#map")
-rescue e : Selenium::Error
-  exit
+      # Adjust `.credits` section
+      document_manager.execute_script("document.getElementsByClassName('credits')[0].style.setProperty('bottom', '7%')")
+      document_manager.execute_script("document.querySelector('.credits h2').style.display = 'none'")
+      document_manager.execute_script("document.getElementsByClassName('credits')[0].style.setProperty('font-size', 'xx-large')")
+
+      tempfile = File.tempfile("alers_wallpaper", ".png")
+
+      element.screenshot(tempfile.path)
+
+      tempfile
+    end
+
+    def finalize
+      @session.delete
+    end
+  end
 end
 
-document_manager = session.document_manager
-
-# Switch to dark theme
-document_manager.execute_script("document.getElementsByTagName('html')[0].classList.toggle('light')")
-
-# Adjust `.credits` section
-document_manager.execute_script("document.getElementsByClassName('credits')[0].style.setProperty('bottom', '7%')")
-document_manager.execute_script("document.querySelector('.credits h2').style.display = 'none'")
-document_manager.execute_script("document.getElementsByClassName('credits')[0].style.setProperty('font-size', 'xx-large')")
-
-tempfile = File.tempfile("alers_wallpaper", ".png")
-
-element.screenshot(tempfile.path)
-session.delete
-
-if tempfile.size.zero?
-  tempfile.delete
-
-  exit
-end
+browser = AirAlertMapUaWallpaper::Browser.new
+tempfile = browser.take_screenshot
 
 # https://gist.github.com/mamantoha/c01363e5c791e8324d6248b09cf29bbb
 set_wallpaper_script = <<-JS
