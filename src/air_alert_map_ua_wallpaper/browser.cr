05f168c9ef35c52d4d4178a44326e8ca0d696582
Anton Maminov <anton.linux@gmail.com>
2023-02-27 22:46:17 +0200

    full map version

--- src/air_alert_map_ua_wallpaper/browser.cr
+++ src/air_alert_map_ua_wallpaper/browser.cr
@@ -21,9 +21,9 @@ module AirAlertMapUaWallpaper
       map_url =
         case language
         in Lang::Uk
-          "https://alerts.in.ua?minimal&disableInteractiveMap&showWarnings"
+          "https://alerts.in.ua?minimal&disableInteractiveMap&full&showWarnings"
         in Lang::En
-          "https://alerts.in.ua/en?minimal&disableInteractiveMap&showWarnings"
+          "https://alerts.in.ua/en?minimal&disableInteractiveMap&full&showWarnings"
         end
 
       @session.navigate_to(map_url)
