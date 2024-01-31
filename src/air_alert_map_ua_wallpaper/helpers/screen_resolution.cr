0cff44d4bdfadb5c75261749066dbefdd7a1e40e
Anton Maminov <anton.linux@gmail.com>
2024-01-31 15:49:54 +0200

    get screen resolution on KDE Plasma

--- src/air_alert_map_ua_wallpaper/helpers/screen_resolution.cr
+++ src/air_alert_map_ua_wallpaper/helpers/screen_resolution.cr
@@ -24,6 +24,26 @@ module AirAlertMapUaWallpaper
 
         {width: width, height: height}
       end
+    {% elsif flag?(:linux) %}
+      def get_screen_resolution
+        output = `qdbus org.kde.KWin /KWin org.kde.KWin.supportInformation`
+
+        geometry_regex = /\n+Geometry: \d+,\d+,(\d+)x(\d+)\n/
+        scale_regex = /\nScale: (\d+(?:\.\d+)?)\n/
+
+        geometry_match = geometry_regex.match(output)
+        scale_match = scale_regex.match(output)
+
+        result =
+          if geometry_match && scale_match
+            width = geometry_match[1].to_i * scale_match[1].to_f
+            height = geometry_match[2].to_i * scale_match[1].to_f
+
+            {width: width.to_i, height: height.to_i}
+          else
+            nil
+          end
+      end
     {% else %}
       def get_screen_resolution; end
     {% end %}
