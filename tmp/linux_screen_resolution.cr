7a8daccfb365f7e9f94477dee16e878023903144
Anton Maminov <anton.linux@gmail.com>
2024-08-19 14:08:31 +0300

    add tmp/linux_screen_resolution.cr

---
+++ tmp/linux_screen_resolution.cr
@@ -0,0 +1,16 @@
+resolution = ""
+
+Dir.glob("/sys/class/drm/*/modes", follow_symlinks: true).each do |dev|
+  single_resolution = File.open(dev) { |file| file.gets_to_end.split("\n").first }
+
+  unless single_resolution.nil? || single_resolution.empty?
+    p! dev
+    resolution = "#{single_resolution}, #{resolution}"
+  end
+end
+
+p! resolution
+# Remove the trailing comma and space from the resolution string
+resolution = resolution.chomp(", ")
+
+puts resolution
